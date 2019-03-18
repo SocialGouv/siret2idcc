5a151ddc558c0b0b334c1a54d5704a8b02003968
Adrien Di Pasquale <adrien@dipasquale.fr>
2019-03-18 14:58:07 +0100

    import and expose idcc titles from dila2sql api

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 import pandas as pd
 import os
 from sqlalchemy import create_engine
-
+import requests
 
 ORIGINAL_FILEPATH = "./extraction_etablissements_idcc.csv"
 
@@ -23,29 +23,46 @@ def filter_df(df):
     print("filtering and reformatting...")
     df_france = df[df["Pays"] == "France"]
     df_simple = df_france. \
-        rename(columns={"Nom de l'établissement": 'name'}) \
-        .filter(items=['SIRET', 'name', 'IDCC'])
+        rename(columns={
+            "Nom de l'établissement": 'name',
+            "SIRET": 'siret',
+            'IDCC': 'idcc_num'
+        }) \
+        .filter(items=['siret', 'name', 'idcc_num'])
     print(df_simple.head())
     return df_simple
 
+def get_num_to_titles_df():
+    dila2sql_api_url = "https://api.dila2sql.num.social.gouv.fr";
+    req = requests.get("%s/v1/base/KALI/conteneurs?nature=IDCC" % dila2sql_api_url)
+    num_to_title = {}
+    for conteneur in req.json():
+        num_to_title[conteneur["num"]] = conteneur["titre"]
+    return pd.DataFrame.from_dict({
+        "num": list(num_to_title.keys()),
+        "titre": list(num_to_title.values())
+    })
 
-def output_to_csv(df):
+def output_to_csv(siret_to_idcc_df, num_to_titles_df):
     print("exporting to CSV ....")
-    csv_path = './siret_idcc.csv'
-    df.to_csv(csv_path, index=False)
-    print("exported %s rows to %s" % (df.shape[0], csv_path))
+    siret_to_idcc_csv_path = './siret_idcc.csv'
+    siret_to_idcc_df.to_csv(siret_to_idcc_csv_path, index=False)
+    print("exported %s rows to %s" % (siret_to_idcc_df.shape[0], siret_to_idcc_csv_path))
+    num_to_title_csv_path = './idcc_titles.csv'
+    num_to_titles_df.to_csv(num_to_title_csv_path, index=False)
+    print("exported %s IDCCs to %s" % (num_to_titles_df.shape[0], num_to_title_csv_path))
 
-
-def output_to_sqlite(df):
+def output_to_sqlite(siret_to_idcc_df, num_to_titles_df):
     print("exporting to SQLite ...")
     sqlite_path = "siret_idcc.sqlite"
     if os.path.exists(sqlite_path):
         os.remove(sqlite_path)
     engine = create_engine('sqlite:///%s' % sqlite_path)
-    df.to_sql('companies', con=engine, index=False)
-    engine.execute("CREATE INDEX companies_siret_idx ON companies(SIRET);")
-    print("exported %s to %s!" % (sqlite_path, df.shape[0]))
-
+    siret_to_idcc_df.to_sql('companies', con=engine, index=False)
+    num_to_titles_df.to_sql('idcc', con=engine, index=False)
+    engine.execute("CREATE INDEX companies_siret_idx ON companies(siret);")
+    engine.execute("CREATE INDEX idcc_num_idx ON idcc(num);")
+    print("exported %s rows to %s!" % (sqlite_path, siret_to_idcc_df.shape[0]))
 
 if __name__ == '__main__':
     arg_parser = ArgumentParser()
@@ -53,10 +70,11 @@ if __name__ == '__main__':
     args = arg_parser.parse_args()
 
     df = read_original_csv()
-    filtered_df = filter_df(df)
+    siret_to_idcc_df = filter_df(df)
+    num_to_titles_df = get_num_to_titles_df()
 
     if args.output == "csv":
-        output_to_csv(filtered_df)
+        output_to_csv(siret_to_idcc_df, num_to_titles_df)
     elif args.output == "sqlite":
-        output_to_sqlite(filtered_df)
+        output_to_sqlite(siret_to_idcc_df, num_to_titles_df)
 
--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const betterSQLite3 = require('better-sqlite3');
 const SQLITE_PATH = './data/siret_idcc.sqlite';
 
 const db = betterSQLite3(SQLITE_PATH);
-const query = db.prepare('SELECT * FROM companies WHERE SIRET=?')
+const query = db.prepare(`
+  SELECT companies.siret, companies.name, companies.idcc_num, idcc.titre
+  FROM companies
+  LEFT JOIN idcc ON companies.idcc_num = idcc.num
+  WHERE companies.siret = ?
+`)
 
 const app = express()
 app.use(cors())
@@ -26,10 +31,12 @@ app.get(
     }
 
     const baseCompany = {
-      'siret': matches[0].SIRET,
-      'name': matches[0].name
+      'siret': matches[0].siret,
+      'name': matches[0].name,
     };
-    const idccList = matches.map(m => m.IDCC).filter(idcc => idcc !== "9999");
+    const idccList = matches.
+      filter(match => match.idcc_num !== "9999").
+      map(match => ({num: match.idcc_num, titre: match.titre}));
     return res.json({ company: { ...baseCompany, idccList }});
   }
 )
