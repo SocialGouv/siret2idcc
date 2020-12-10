1fbf5c0e1acde95160808ee5b561f88f77d86c06
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-12-10 18:13:04 +0100

    fix RANCHER_PROJECT_ID

--- .k8s/__tests__/kosko generate --env dev.ts
+++ .k8s/__tests__/kosko generate --env dev.ts
@@ -5,6 +5,7 @@ import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 
 jest.setTimeout(1000 * 60);
 test("kosko generate --dev", async () => {
+  process.env.RANCHER_PROJECT_ID = "c-kk8xm:p-2h6lh";
   expect(
     await getEnvManifests("dev", "", project("siret2idcc").dev)
   ).toMatchSnapshot();
