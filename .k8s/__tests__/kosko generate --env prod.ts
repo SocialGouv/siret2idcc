e0e836202eef7a24fafc1377488ef6ed528c9922
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-12-11 18:04:18 +0100

    feat(k8s): use @socialgouv/kosko-charts@alpha (#129)
    Co-authored-by: Douglas DUTEIL <douglasduteil@gmail.com>

---
+++ .k8s/__tests__/kosko generate --env prod.ts
@@ -0,0 +1,13 @@
+//
+
+import { getEnvManifests } from "@socialgouv/kosko-charts/testing";
+import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
+
+jest.setTimeout(1000 * 60);
+test("kosko generate --prod", async () => {
+  expect(
+    await getEnvManifests("prod", "'!(_*)'", {
+      ...project("siret2idcc").prod,
+    })
+  ).toMatchSnapshot();
+});
