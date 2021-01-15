0a52ca128f22b6f090779130ce3a8b007252c3b8
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-01-16 00:13:55 +0100

    chore(k8s): rename deployment

--- .k8s/components/siret2idcc.ts
+++ .k8s/components/siret2idcc.ts
@@ -2,10 +2,11 @@ import env from "@kosko/env";
 
 import { create } from "@socialgouv/kosko-charts/components/app";
 
-const manifests = create("app", {
+const manifests = create("siret2idcc", {
   env,
   config: { containerPort: 3000 },
   deployment: {
+    image: `harbor.fabrique.social.gouv.fr/cdtn/siret2idcc:${process.env.CI_COMMIT_SHA}`,
     container: {
       resources: {
         requests: {
