2a6de3d6bbaf298b71cfc314004a666a353996aa
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-01-18 23:31:58 +0100

    fix(ci): fix preprod/prod tag (#136)

--- .k8s/components/siret2idcc.ts
+++ .k8s/components/siret2idcc.ts
@@ -2,11 +2,15 @@ import env from "@kosko/env";
 
 import { create } from "@socialgouv/kosko-charts/components/app";
 
+const IMAGE_TAG = process.env.CI_COMMIT_TAG
+  ? process.env.CI_COMMIT_TAG.replace(/^v/, "")
+  : process.env.CI_COMMIT_SHA;
+
 const manifests = create("siret2idcc", {
   env,
   config: { containerPort: 3000 },
   deployment: {
-    image: `harbor.fabrique.social.gouv.fr/cdtn/siret2idcc:${process.env.CI_COMMIT_SHA}`,
+    image: `harbor.fabrique.social.gouv.fr/cdtn/siret2idcc:${IMAGE_TAG}`,
     container: {
       resources: {
         requests: {
