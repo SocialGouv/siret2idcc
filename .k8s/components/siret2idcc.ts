aa0c36c988e06040eeb4ee2c2c846296fde1c84c
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-01-19 17:00:37 +0100

    chore(ci): rm useless

--- .k8s/components/siret2idcc.ts
+++ .k8s/components/siret2idcc.ts
@@ -3,10 +3,6 @@ import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/app";
 import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
 
-const IMAGE_TAG = process.env.CI_COMMIT_TAG
-  ? process.env.CI_COMMIT_TAG.replace(/^v/, "")
-  : process.env.CI_COMMIT_SHA;
-
 const manifests = create("siret2idcc", {
   env,
   config: { containerPort: 3000 },
