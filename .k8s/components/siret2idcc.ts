c84c87d74d97295f7f816244c876b5e722702928
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-01-19 15:18:40 +0100

    fix: use kosko-charts@4.5

--- .k8s/components/siret2idcc.ts
+++ .k8s/components/siret2idcc.ts
@@ -1,6 +1,7 @@
 import env from "@kosko/env";
 
 import { create } from "@socialgouv/kosko-charts/components/app";
+import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
 
 const IMAGE_TAG = process.env.CI_COMMIT_TAG
   ? process.env.CI_COMMIT_TAG.replace(/^v/, "")
@@ -10,7 +11,7 @@ const manifests = create("siret2idcc", {
   env,
   config: { containerPort: 3000 },
   deployment: {
-    image: `harbor.fabrique.social.gouv.fr/cdtn/siret2idcc:${IMAGE_TAG}`,
+    image: getHarborImagePath({ name: "siret2idcc" }),
     container: {
       resources: {
         requests: {
